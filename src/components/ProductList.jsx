cfdf679a6818c3b4c937377487f48c90242047e7
mizanur rahman <mdmizanurrahman5215@gmail.com>
2025-03-02 21:44:20 +0600

    first commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import GridView from "./GridView";
+import ListView from "./ListView";
+import { useFilterContext } from "../context/FilterContext";
+
+const ProductList = () => {
+   
+  const { filter_products, grid_view } = useFilterContext();
+  console.log( filter_products);
+
+  if (grid_view === true) {
+    return <GridView products={filter_products} />;
+  }
+
+  if (grid_view === false) {
+    return <ListView products={filter_products} />;
+  }
+};
+
+export default ProductList;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App';
+import { AppProvider } from './context/ProductContext';
+import { FilterContextProvider } from './context/FilterContext';
+
+
+createRoot(document.getElementById('root')).render(
+  <AppProvider>
+    <FilterContextProvider>
+    <App/>
+    </FilterContextProvider>
+  </AppProvider>
+
+ 
+)
